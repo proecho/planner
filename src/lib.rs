e283b87af8ba22e58663283df143140e9ba32676
proecho <niamhgillespie2016@outlook.com>
2020-10-08 14:46:49 +0100

    bug fixes

--- src/lib.rs
+++ src/lib.rs
@@ -8,27 +8,30 @@ pub enum Terminator{
 	No,
 }
 
+#[derive(Debug,PartialEq)]
 enum entrys<'a>{
 	Todo(Todo <'a>),
-	Events(Events),
-	appointments(appointments),
+	Events(Events <'a>),
+	appointments(Appointments <'a>),
 }
 
+#[derive(Debug,PartialEq)]
 struct Todo<'a>{
 	Title: Option<&'a str>,
 	List: Option<&'a str>,
 	DateTime: Box<Option<DateTime<Utc>>>,
 }
 
-struct Events{
+#[derive(Debug,PartialEq)]
+struct Events<'a>{
 	Title: Option<&'a str>,
 	DateTime: Box<Option<DateTime<Utc>>>,
 	Description: Option<&'a str>,
 	Attendees:Option<&'a str>,
 }
 	
-
-struct Appointments{
+#[derive(Debug,PartialEq)]
+struct Appointments<'a>{
 	Title: Option<&'a str>,
 	DateTime: Box<Option<DateTime<Utc>>>,
 	With_who: Option<&'a str>,
@@ -38,16 +41,16 @@ struct Appointments{
 
 trait entry_type {
 	fn save(&self) -> Result<Terminator,String> {
-		Err("not implemented".to_string())
+		Ok(Terminator::No)
 		}
 	
 }
 
 impl <'a> entry_type for Todo<'a> {}
 
-impl entry_type for Events {}
+impl <'a> entry_type for Events<'a> {}
 
-impl entry_type for appointments {}
+impl <'a> entry_type for Appointments<'a> {}
 
 impl<'a> Todo <'a> {
 	fn new(Title: Option<&'a str>, List: Option<&'a str>, DateTime: Box<Option<DateTime<Utc>>>) -> Todo<'a>{
@@ -59,8 +62,8 @@ impl<'a> Todo <'a> {
 	}
 }
 
-impl Events {
-	fn new(Title: Option<&'a str>, DateTime: Box<Option<DateTime<Utc>>>, Description: Option<&'a str>, Attendees:Option<&'a str>) -> Events {
+impl<'a> Events<'a> {
+	fn new(Title: Option<&'a str>, DateTime: Box<Option<DateTime<Utc>>>, Description: Option<&'a str>, Attendees:Option<&'a str>) -> Events<'a> {
 		Events{
 			Title,
 			DateTime,
@@ -70,13 +73,13 @@ impl Events {
 	}
 }
 
-impl Appointments{
-	fn new(Title: Option<&'a str>, DateTime: Box<Option<DateTime<Utc>>>, With_who: Option<&'a str>, Description: Option<&'a str>) -> Appointments {
+impl<'a> Appointments<'a>{
+	fn new(Title: Option<&'a str>, DateTime: Box<Option<DateTime<Utc>>>, With_who: Option<&'a str>, Description: Option<&'a str>) -> Appointments<'a> {
 		Appointments{
-			Title: Option<&'a str>,
-	        DateTime: Box<Option<DateTime<Utc>>>,
-	        With_who: Option<&'a str>,
-	        Description: Option<&'a str>,
+			Title, 
+	        DateTime,
+	        With_who,
+	        Description,
         }
      }
 }
@@ -141,13 +144,23 @@ fn input_parser<'a>(input:Vec<&str>) -> Result<entrys<'a>,String> {
 			)))
 		}
 		"Event" =>{
-			Err("not implemented".to_string())
+			return Ok(entrys::Events(Events::new(
+			    None,
+			    Box::new(None),
+			    None,
+			    None,
+			)))
 		}
 		"appointment" =>{
-			Err("not implemented".to_string())
+			return Ok(entrys::appointments(Appointments::new(
+			    None,
+			    Box::new(None),
+			    None,
+			    None,
+			)))
 		}
 	    _ =>{
-			 Err("invalid input".to_string())
+			 return Err("invalid input".to_string())
 		}
 	}
 }	 
@@ -169,7 +182,7 @@ mod tests {
 	#[test]
 	fn doesnt_crash_on_long(){
 		remove_file("test_file.txt");
-		let  a = String::from_utf8(vec![b'P',254]).unwrap();
+		let  a = String::from_utf8(vec![b'P',100]).unwrap();
 		let words = [&a[..]].to_vec();
 		assert_eq!(process(words,"test_file.txt".to_string()), Err("invalid input".to_string()));
 		
@@ -178,14 +191,15 @@ mod tests {
 	fn returns_for_Todo() {
 		remove_file("test_file.txt");
 		let a = "To_do".to_string();
-		let words = [&a[..]].to_vec();
+		let b = "new".to_string();
+		let words = [&b[..],&a[..]].to_vec();
 		assert_eq!(process(words,"test_file.txt".to_string()),Ok(Terminator::No));
 	
 	}
 	#[test]
 	fn input_parser_fails_nonsense() {
 		let input = ["dummy", "feadeafdeasf"].to_vec();
-		assert_eq!(input_parser(input), Err("invalid input".to_string());
+		assert_eq!(input_parser(input), Err("invalid input".to_string()));
 	} 
 		
 	
